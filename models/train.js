7a721d380e472e6b87a30bd0cc823dfa358ddfc0
Tanmay000009 <tanmayvyas009@gmail.com>
2022-11-13 12:23:35 +0530

    changes

--- models/train.js
+++ models/train.js
@@ -1,45 +1,47 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const trainSchema = new mongoose.Schema({
-    trNo:{
-        type:Number,
-        required:true
+  trNo: {
+    type: Number,
+    required: true,
+  },
+  trName: {
+    type: String,
+    required: true,
+  },
+  start: {
+    type: String,
+    required: true,
+  },
+  end: {
+    type: String,
+    required: true,
+  },
+  runsOn: [
+    {
+      days: {
+        type: String,
+        required: true,
+      },
     },
-    trName:{
-        type:String,
-        required:true
-    },
-    start:{
-        type:String,
-        required:true
-    },
-    end:{
-        type:String,
-        required:true
-    },
-    runsOn:[{
-        days:{
-            type:String,
-            required:true
-        },
-    }],
-    date:Date
-})
+  ],
+  date: Date,
+});
 
-const Train = mongoose.model('Train', trainSchema)
+const Train = mongoose.model("Train", trainSchema);
 
 // This is for adding trains to the database ......
-const train = new Train({
-    trName:'Chennai Express',
-    trNo:16,
-    start:'Chennai Jn',
-    end:'Banglore Jn',
-    // runsOn:[{days:'Tuesday'},{days:'Saturday'},{days:'Friday'}],
-    runsOn:[{days:'Wednesday'},{days:'Monday'},{days:'Sunday'},{days:'Saturday'}]
-})
-train.save().then(()=>{
-    //console.log(train)
-}).catch((e)=>{
-    //console.log(e)
-})
-module.exports = Train
+// const train = new Train({
+//     trName:'Chennai Express',
+//     trNo:16,
+//     start:'Chennai Jn',
+//     end:'Banglore Jn',
+//     // runsOn:[{days:'Tuesday'},{days:'Saturday'},{days:'Friday'}],
+//     runsOn:[{days:'Wednesday'},{days:'Monday'},{days:'Sunday'},{days:'Saturday'}]
+// })
+// train.save().then(()=>{
+//     //console.log(train)
+// }).catch((e)=>{
+//     //console.log(e)
+// })
+module.exports = Train;
--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -1,88 +1,94 @@
-const Booking = require('../models/Booking')
-const express = require('express')
-const router = new express.Router
-const { ensureAuthenticated } = require('../auth')
-const Train = require('../models/train')
-const nodemailer = require('nodemailer')
+const Booking = require("../models/Booking");
+const express = require("express");
+const router = new express.Router();
+const { ensureAuthenticated } = require("../auth");
+const Train = require("../models/train");
+const nodemailer = require("nodemailer");
 
-router.get('/login/train_details', async (req, res) => {
-    const take = req.query
-    const day = get(take.date)
-    const train = await Train.find({ start: take.start, end: take.end, 'runsOn.days': day })
-    for (var i = 0; i < train.length; i++) {
-        var obj = train[i]
-        Object.assign(obj, { date: take.date });
-    }
-    try {
-        res.render('booking', {
-            train
-        })
-    } catch (e) {
-        res.render(e)
-    }
-})
+router.get("/login/train_details", async (req, res) => {
+  const take = req.query;
+  const day = get(take.date);
+  const train = await Train.find({
+    start: take.start,
+    end: take.end,
+    "runsOn.days": day,
+  });
+  for (var i = 0; i < train.length; i++) {
+    var obj = train[i];
+    Object.assign(obj, { date: take.date });
+  }
+  try {
+    res.render("booking", {
+      train,
+    });
+  } catch (e) {
+    res.render(e);
+  }
+});
 
-router.post('/login/book', ensureAuthenticated, async (req, res) => {
-    console.log(req.user);
-    const book = new Booking({
-        ...req.body,
-        owner: req.user._id
-    })
-    try {
-        await book.save()
-        let transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-              user: 'railways_22@outlook.com', // generated ethereal user
-              pass: 'R@ilways', // generated ethereal password
-            },
-          });
+router.post("/login/book", ensureAuthenticated, async (req, res) => {
+  console.log("body", req.body);
+  const book = new Booking({
+    ...req.body,
+    owner: req.user._id,
+  });
+  console.log(book);
+  try {
+    await book.save();
+    const mailOptions = {
+      from: `Admin railways_22@outlook.com`,
 
-          let info = await transporter.sendMail({
-            from: 'Admin', // sender address
-            to: req.user.email, // list of receivers
-            subject: "Ticket Booked Succedfully!" , // Subject line
-            text: `Your booking is generated successfully, booking id: ${book._id}`
-          });
-          console.log("Message sent: %s", info.messageId);
-        res.render('book',{
-            book
-        })
-    } catch (e) {
-        res.send(e)
-    }
-})
+      to: req.user.email,
+      subject: "Booking confirm",
+      html: `<p>Your booking is confirmed with booking id <strong>${book._id}</strong> </p>`,
+    };
+    const transporter = nodemailer.createTransport({
+      service: "outlook",
+      auth: {
+        user: "railways_22@outlook.com",
+        pass: "R@ilways",
+      },
+    });
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+        return res.status(500).json({
+          error: "Internal server error",
+        });
+      }
+    });
+    console.log("Email sent: " + transporter.response);
+
+    res.render("book", {
+      book,
+    });
+  } catch (e) {
+    res.send(e);
+  }
+});
 
 const get = (date) => {
-    const dayGet = new Date(date.toString());
-    const day = dayGet.getDay();
-    if (day === 0) {
-        return 'Sunday'
-    }
-    else if (day === 1) {
-        return 'Monday'
-    }
-    else if (day === 2) {
-        return 'Tuesday'
-    }
-    else if (day === 3) {
-        return 'Wednesday'
-    }
-    else if (day === 4) {
-        return 'Thursday'
-    }
-    else if (day === 5) {
-        return 'Friday'
-    }
-    else if (day === 6) {
-        return 'Saturday'
-    }
-}
+  const dayGet = new Date(date.toString());
+  const day = dayGet.getDay();
+  if (day === 0) {
+    return "Sunday";
+  } else if (day === 1) {
+    return "Monday";
+  } else if (day === 2) {
+    return "Tuesday";
+  } else if (day === 3) {
+    return "Wednesday";
+  } else if (day === 4) {
+    return "Thursday";
+  } else if (day === 5) {
+    return "Friday";
+  } else if (day === 6) {
+    return "Saturday";
+  }
+};
 
-module.exports = router
-
+module.exports = router;
 
 // railways_22@outlook.com
 // R@ilways
